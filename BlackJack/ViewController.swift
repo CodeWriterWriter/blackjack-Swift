91bf8fd4b981ae5e1fa2f5bef5d2e80ff1ff5ee3
20063321 <20063321@it102imac17.wit.ie>
2015-11-25 17:16:41 +0000

    Initial Commit

---
+++ BlackJack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlackJack
+//
+//  Created by 20063321 on 25/11/2015.
+//  Copyright © 2015 20063321. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
